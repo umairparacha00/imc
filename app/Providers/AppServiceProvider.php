45959b8c84c1d99cdc7c390323634d484d952eb4
Umair Paracha <umairparacha0047@gmail.com>
2021-04-06 10:15:42 +0500

    Trusted Proxies fixed2

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use Illuminate\Routing\UrlGenerator;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -16,13 +18,11 @@ class AppServiceProvider extends ServiceProvider
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
-    public function boot()
-    {
-        //
-    }
+	/**
+	 * Bootstrap any application services.
+	 *
+	 * @param UrlGenerator $url
+	 * @return void
+	 */
+    public function boot(UrlGenerator $url) { URL::forceScheme('https'); }
 }
